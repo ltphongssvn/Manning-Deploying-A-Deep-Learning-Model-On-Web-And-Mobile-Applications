dc4b01c80a8d6ed1f508abbdef3d249de69b6f56
Thanh Phong Le <ltphongssvn@gmail.com>
2026-03-04 18:57:33 -0800

    Fix SafeAreaView deprecation: use react-native-safe-area-context

--- mobile/HomeScreen.tsx
+++ mobile/HomeScreen.tsx
@@ -9,8 +9,8 @@ import {
   TouchableOpacity,
   ActivityIndicator,
   Alert,
-  SafeAreaView,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import * as ImagePicker from "expo-image-picker";
 import { initializeTF, loadModel, isModelReady, classifyImage } from "./ModelService";
 import type { ClassificationResult } from "./ModelService";
